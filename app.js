b880000e663708a7b16d9b5629619c46f8db12e2
Mehrdad <43243084+mehrdadchatrazar@users.noreply.github.com>
2023-01-05 23:46:49 +0330

    implement playRound function

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
 function getComputerChoice(){
-    let number = Math.floor(Math.random()*150) + 1;
-    console.log(number);
-    if(number <= 50){
-        return "Rock";
-    } else if (number <= 100){
-        return "Paper";
+    let choice = Math.floor(Math.random()*3) + 1;
+
+    if(choice == 1){
+        return "rock";
+    } else if (choice == 2){
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
+
+
+function playRound(playerChoice, computerChoice){
+    if (playerChoice == "rock" && computerChoice == "rock") {
+        return "It's a Draw!";
+    }
+    if (playerChoice == "rock" && computerChoice == "paper") {
+        return "You Lose! Paper beats Rock";
+    }
+    if (playerChoice == "rock" && computerChoice == "scissors") {
+        return "You Win! Rock beats Scissors";
+    }
+
+    if (playerChoice == "paper" && computerChoice == "rock") {
+        return "You Win! Paper beats Rock";
+    }
+    if (playerChoice == "paper" && computerChoice == "paper") {
+        return "It's a Draw!";
+    }
+    if (playerChoice == "paper" && computerChoice == "scissors") {
+        return "You Lose! Scissors beats Paper";
+    }
+
+    if (playerChoice == "scissors" && computerChoice == "rock") {
+        return "You Lose! Rock beats Scissors";
+    }
+    if (playerChoice == "scissors" && computerChoice == "paper") {
+        return "You Win! Scissors beats Paper";
+    }
+    if (playerChoice == "scissors" && computerChoice == "scissors") {
+        return "It's a Draw!";
+    }
+    
+}
+
